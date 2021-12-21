dae76a60c2854b58f415382695e0e77b9234598a
Carlos Antonio Balbiani Mesa <carlos.balbiani@resolvit.com>
2021-12-21 16:50:37 -0300

    fixed invoice

--- entity/invoice.go
+++ entity/invoice.go
@@ -7,7 +7,6 @@ import (
 type Invoice struct {
 	Id          uint      `gorm:"primaryKey"`
 	ClientId    uint      `gorm:"index"`
-	ClientName  string    `gorm:"not null;default:null"`
 	InvoiceDate time.Time ``
 	Products    []Product `json:",omitempty"`
 }
